bb22e26196dfd6f91598d83b839f03528bbcb09f
Carlos "Café Café" García <58571285+Carlos-CafeCafe@users.noreply.github.com>
2024-08-18 22:50:32 -0300

    cleaner code

--- main.ts
+++ main.ts
@@ -1,23 +1,12 @@
 enum RadioMessage {
     message1 = 49434
 }
-function detenerAlerta () {
-    let bandera = 0
-    alerta = 0
-    music.stopAllSounds()
-    radio.sendValue("alerta", alerta)
-    basic.showNumber(bandera)
-}
-input.onButtonPressed(Button.AB, function () {
-    detenerAlerta()
-})
 radio.onReceivedValue(function (name, value) {
     radio.sendValue(name, value)
     basic.showIcon(IconNames.Yes)
     basic.pause(500)
     basic.clearScreen()
 })
-let alerta = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.showIcon(IconNames.Heart)
